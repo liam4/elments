ffb695624bb6dc2bf989fcb752349349d24691c0
liam4 <towerofnix@gmail.com>
2016-03-02 14:21:40 -0400

    Another commit

--- demo.js
+++ demo.js
@@ -1,17 +1,39 @@
-elm.from('body')
-    .add(elm.create('h1')
-       .add(elm.text('Hello')))
-   .add(elm.create('p')
-       .add(elm.text('This is a paragraph')));
+elm.from('head')
 
-/*
-<body>
-    ...
-    <h1>
-        Hello
-    </h1>
-    <p>
-        This is a paragraph
-    </p>
-</body>
-*/
+  // <link rel='stylesheet' href='colors.css' />
+  .add(elm.create('link')
+    .attr('rel', 'stylesheet')
+    .attr('href', 'colors.css'));
+
+// <body>
+elm.from('body')
+
+  // <h1>Hello</h1>
+  .add(elm.create('h1')
+    .add(elm.text('Hello')))
+
+  // <p><i>In a world where we don't use HTML templates...</i> Use elm!</i></p>
+  .add(elm.create('p')
+    .add(elm.create('i').add(elm.text('In a world where we don\'t use HTML templates...')))
+    .add(elm.text(' Use elm!')))
+
+  // <button>Don't click me</button>
+  // Does something when it's clicked
+  .add(elm.create('button')
+    .add(elm.text('Don\'t click me'))
+    .on('click', function() {
+      alert('How would you like it if I clicked you?');
+    }))
+
+  // <div>Don't even think about hovering over me!</div>
+  // Does something when the mouse is moved over it
+  .add(elm.create('div')
+    .add(elm.text('Don\'t even think about hovering over me!'))
+    .on('mouseover', function() {
+      alert('D:');
+    }))
+
+  // <p class='red'>Are you feeling red today?</p>
+  .add(elm.create('p')
+    .class('red')
+    .add(elm.text('Are you feeling red today?')));
--- elments.js
+++ elments.js
@@ -1,69 +1,73 @@
-var elm = {};
+var elm = (function() {
+  var elm = {};
 
-elm.from = function(query) {
-    return elm.assign(document.querySelector(query));
-};
-
-elm.assign = function(el) {
-
-    var elment = {
-        el: el
+  if (typeof Symbol === 'undefined') {
+    var isIterable = function(o) {
+      return o instanceof Array;
     };
+  } else {
+    var isIterable = function(o) {
+      return typeof o[Symbol.iterator] !== 'undefined';
+    };
+  }
+
+  var assign = function(el) {
+    var elment = {el: el};
 
     elment.add = function(anElment) {
-        console.log('add', anElment);
-        elment.el.appendChild(anElment.el);
-        return elment;
+      elment.el.appendChild(anElment.el);
+      return elment;
+    };
+
+    elment.on = function(listenerName, listenerValue) {
+      var callbackTypeError = TypeError('Callback for event listener must be function or array of functions');
+      if (listenerValue instanceof Function) {
+        el.addEventListener(listenerName, listenerValue);
+      } else if (isIterable(listenerValue)) {
+        for (var callback of listenerValue) {
+          if (callback instanceof Function) {
+            el.addEventListener(listenerName, callback);
+          } else {
+            throw callbackTypeError;
+          }
+        }
+      } else {
+        throw callbackTypeError;
+      }
+      return elment;
+    };
+
+    elment.apply = function(fn) {
+      return fn(elment);
+    };
+
+    elment.attr = function(attributeName, attributeValue) {
+      elment.el.setAttribute(attributeName, attributeValue);
+      return elment;
+    };
+
+    elment.class = function(className) {
+      elment.el.classList.add(className);
+      return elment;
     };
 
     return elment;
-};
+  };
 
-elm.create = function(tag, attributes, listeners) {
+  elm.from = function(query) {
+    return assign(document.querySelector(query));
+  };
 
+  elm.create = function(tag, attributes, listeners) {
     if (typeof tag === 'undefined')
-        throw new TypeError('elm requires more than 0 arguments');
-    if (typeof attributes === 'undefined')
-        attributes = {};
-    if (typeof listeners === 'undefined')
-        listeners = {};
+      throw new TypeError('elm requires more than 0 arguments');
 
-    if (typeof Symbol === 'undefined') {
-        var isIterable = function(o) {
-            return o instanceof Array;
-        };
-    } else {
-        var isIterable = function(o) {
-            return typeof o[Symbol.iterator] !== 'undefined';
-        };
-    }
+    return assign(document.createElement(tag));
+  };
 
-    var callbackTypeError = TypeError('Callback for event listener must be function or array of functions');
-
-    var el = document.createElement(tag);
-    for (var attributeName in attributes) {
-        el.setAttribute(attributeName, attributes[attributeName]);
-    }
-    for (var listenerName in listeners) {
-        var listenerValue = listeners[listenerName];
-        if (listenerValue instanceof Function) {
-            el.addEventListener(listenerName, listenerValue);
-        } else if (isIterable(listenerValue)) {
-            for (var callback of listenerValue) {
-                if (callback instanceof Function) {
-                    el.addEventListener(listenerName, callback);
-                } else {
-                    throw callbackTypeError;
-                }
-            }
-        } else {
-            throw callbackTypeError;
-        }
-    }
-
-    return elm.assign(el);
-};
-
-elm.text = function(text) {
+  elm.text = function(text) {
     return {el: document.createTextNode(text)};
-};
+  };
+
+  return elm;
+}());
